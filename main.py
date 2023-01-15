459055d4ef7cff8848ad715397abd6b6a138921c
BearCat87 <@>
2023-01-15 21:34:11 +0900

    init

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 import streamlit as st
+from google.oauth2 import service_account
+from gsheetsdb import connect
 
-st.title("Streamlet Test")
-st.write("asdf")
+
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+path = "https://docs.google.com/spreadsheets/d/1-W9-Csw1YjlhwXitR30vCZzUAcb6JqHu2fuueXDFC6c/edit#gid=0"
+sheet_url = st.secrets[path]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.name} has a :{row.pet}:")
